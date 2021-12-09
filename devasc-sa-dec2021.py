fd3d2afbc3cc1215faad0a8ae3a3fb0adeb8deaf
Chotipat Pornavalai <chotipat@it.kmitl.ac.th>
2021-12-09 19:32:44 +0700

    add .gitignore and add objectives

--- devasc-sa-dec2021.py
+++ devasc-sa-dec2021.py
@@ -7,12 +7,13 @@
 # Instruction
 # 1. Complete all 12 tasks below by adding code to all <!!!REPLACEME!!!!> in this file within 1:30 hour
 # 2. Append your name to this file name (e.g. devasc-sa-dec2021-chotipat.py)
-# 3. Clone reporitory from https://github.com/chotipat/ITCDevNetFinal2021.git
-# 4. Create a private repository in github.com, named "DevnetFinalDec2021". 
+# 3. Clone or fork repository from https://github.com/chotipat/ITCDevNetFinal2021.git to your local computer
+# 4. If necessary, create a private repository in github.com, named "ITCDevnetFinal2021". 
 # 5. Add chotipat@it.kmitl.ac.th as a collaborator in your github repository.
-# 6. Commit this file to your local repository once in a while (e.g. every 15-30 mins).
-# 7. Push your local repository to your github repository when finished.
-# 8. As a backup route, please (attach) this file and send email to chotipat@it.kmitl.ac.th by 17:00.
+# 6. If necessary, clone or create local respository in your computer.
+# 7. Commit this file to your local repository once in a while (e.g. every 15-20 mins).
+# 8. Push your local repository to your github repository when finished.
+# 9. As a backup route, please (attach) this file and send email to chotipat@it.kmitl.ac.th by 17:00.
 #######################################################################################
 # This program:
 # - Uses the hard-coded access token.
