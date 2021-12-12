21c42029c95003f57acf01812f2c5d1a84aeddc0
chotipat-po <87926729+chotipat-po@users.noreply.github.com>
2021-12-12 11:53:28 +0700

    Point all instructions to README.md

--- devasc-sa-dec2021.py
+++ devasc-sa-dec2021.py
@@ -1,36 +1,13 @@
 #######################################################################################
-# Name/Surname:
+# FirstName/Surname:
 # Netacad username:
 # Academy name:
 # Github repository URL: 
 #######################################################################################
 # Instruction
-# Reads README.md in https://github.com/chotipat/ITCDevNetFinal2021
-#######################################################################################
-# This program:
-# - Uses the hard-coded access token.
-# - Monitors the ITCProctoredFinal2021 Webex Team room every second for "/yourname location" message.
-# - Extract the location (city name) from a message starting with “/yourname location” (e.g. /chotipat Washington, DC -> Washington, DC).
-# - Discovers GPS coordinates (latitude and longitude) for the "location" using MapQuest API.
-# - Discovers the current weather description and temperature in degree Celsius of the specified latitude and longitude using openweathermap API. 
-# - Formats and sends the results back to the ITCProctoredFinal2021 Webex Teams room
-#
-# The examinee will:
-# 1. Import libraries for API requests, JSON formatting, and time.
-# 2. Assign the Webex hard-coded access token to the variable accessToken.
-# 3. Prepare GetParameters to get the latest message for messages API.
-# 4. Provide the URL to the Webex Teams messages API, and extract location from the received message.
-# 5. Provide your MapQuest API consumer key.
-# 6. Provide the URL to the MapQuest address API.
-# 7. Provide the MapQuest key values for latitude and longitude.
-# 8. Prepare openweatherAPIGetParameters for OpenWeather API; current weather data for one location by geographic coordinates.
-# 9. Provide the URL to the OpenWeather API; current weather data for one location.
-# 10. Complete the code to get weather description and weather temperature.
-# 11. Complete the code to format the response message.
-# 12. Complete the code to post the message to the Webex Teams room.  
+# Reads README.md in https://github.com/chotipat/ITCDevNetFinal2021 for more information.
 #######################################################################################
  
-
 #######################################################################################
 # 1. Import libraries for API requests, JSON formatting, and time.
 
